5355e4dd5cc4084d292ad9b2cf4605f3c6c830a7
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-04-17 14:36:52 +0200

    Add files via upload

---
+++ Team-42_web_Service/IService1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Team_42_web_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        bool RegisterRec(string name, string email,string Dob, string password); 
+        
+        [OperationContract]
+        bool PostJob(string description, string date, string R_Id,string company,string Location,string qualification,string salary,string experience,string deadline);
+
+        [OperationContract]
+        List<JobPost> GetJobs();
+
+
+        [OperationContract]
+
+        bool LoginRec(string email, string password);
+
+         [OperationContract]
+
+        Recruiter getRec(string email, string password);
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ Team-42_web_Service/Service1.svc.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Team_42_web_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        public List<JobPost> GetJobs()
+        {
+            var list = new List<JobPost>();
+
+            dynamic v = (from u in db.JobPosts select u);
+
+            foreach (JobPost i in v)
+            {
+                list.Add(i);
+
+            }
+
+            return list;
+        }
+
+        public Recruiter getRec(string email, string password)
+        {
+            dynamic R = (from u in db.Recruiters where u.R_Email.Equals(email) && u.R_Password.Equals(password) select u).FirstOrDefault();
+
+            return R;
+        }
+
+        public bool LoginRec(string email, string password)
+        {
+            dynamic r = (from u in db.Recruiters where u.R_Email.Equals(email) && u.R_Password.Equals(password) select u).FirstOrDefault();
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+                return false;
+            }
+        }
+
+        public bool PostJob(string description, string date, string R_Id, string company, string Location, string qualification, string salary, string experience, string deadline)
+        {
+            var c = new JobPost
+            {
+                P_Description = description,
+                P_Date = date,
+                R_Id = R_Id,
+                 Company= company,
+                 Location=Location,
+                 Qualification  =qualification,
+                 Salary=salary,
+                 Experience=experience,
+                 Deadline=deadline,
+
+
+            };
+
+            db.JobPosts.InsertOnSubmit(c);
+            try
+            {
+
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+                return false;
+            }
+        }
+
+        public bool RegisterRec(string name, string email, string Dob, string password)
+        {
+            var c = new Recruiter
+            {
+                R_Name = name,
+                R_Email = email,
+                R_Password = password,
+                R_DOB=Dob
+
+
+            };
+
+            db.Recruiters.InsertOnSubmit(c);
+            try
+            {
+
+                db.SubmitChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                e.GetBaseException();
+                return false;
+            }
+        }
+    }
+}
